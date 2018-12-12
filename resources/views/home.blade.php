ff0d6b6da4d42cab4a39ab441b95d0b30aa71259
Tsuki <Tsuki@192.168.12.92>
2018-12-12 17:40:07 +0100

    init

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title>Mi Web</title>
+        <style>
+			.container {
+				padding: 4em 7em;
+			}
+			h1 {
+				margin: 1em;
+			}
+			hr {
+				margin: 1em 0;
+			}
+		</style>
+	</head>
+    <body>
+		<div class="container">
+			<h1>Hola mundo! ^^</h1>
+			<hr>
+		</div>
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', function () {
+    return view('home');
+});
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/logout', function () {
+    return view('logout');
+});
+Route::get('/catalog', function () {
+    return view('catalog');
+});
+Route::get('/catalog/show/{id}', function ($id) {
+    return view('show');
+});
+Route::get('/catalog/create', function () {
+    return view('create');
+});
+Route::get('/catalog/edit/{id}', function () {
+    return view('edit');
+});
